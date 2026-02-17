03251b690232ebfba0d6d75563519103377d3ebe
KimCheulOck <ansolove1225@gmail.com>
2026-02-17 17:58:15 +0900

    확장메소드 추가

---
+++ Extension/ExtensionGameObject.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public static class ExtensionGameObject
+{
+    public static bool SafeIsNull(this Object @object)
+    {
+        if (@object == null || @object.Equals(null))
+            return true;
+
+        return false;
+    }
+
+    public static bool SafeIsNull(this Object[] @objects)
+    {
+        if (@objects == null || @objects.Equals(null))
+            return true;
+
+        return false;
+    }
+
+    public static void SafeSetActive(this Component component, bool active)
+    {
+        if (component.SafeIsNull())
+            return;
+
+        if (component.gameObject.SafeIsNull())
+            return;
+
+        component.gameObject.SetActive(active);
+    }
+
+    public static void SafeSetActive(this Component[] components, bool active)
+    {
+        if (components.SafeIsNull())
+            return;
+
+        for (int i = 0; i < components.Length; ++i)
+            components[i].SafeSetActive(active);
+    }
+
+    public static void ChangeLayer(this MonoBehaviour mono, string name, bool isChild)
+    {
+        if (mono == null)
+            return;
+
+        mono.gameObject.layer = LayerMask.NameToLayer(name);
+
+        Transform[] childs = mono.GetComponentsInChildren<Transform>(true);
+        for (int i = 0; i < childs.Length; ++i)
+        {
+            if (childs[i] == null)
+                continue;
+
+            childs[i].gameObject.layer = LayerMask.NameToLayer(name);
+        }
+    }
+}
---
+++ Extension/ExtensionGraphic.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class ExtensionGraphic
+{
+    #region Sprite
+    public static void SafeSetColor(this Graphic graphic, Color color)
+    {
+        if (graphic.SafeIsNull())
+            return;
+
+        graphic.color = color;
+    }
+
+    public static void SafeSetSprite(this Image image, Sprite sprite)
+    {
+        if (image.SafeIsNull())
+            return;
+
+        image.sprite = sprite;
+    }
+
+    public static void SafeSetSpriteNativeSize(this Image image, string path)
+    {
+        if (image.SafeIsNull())
+            return;
+
+        image.sprite = Resources.Load<Sprite>(path);
+        image.SetNativeSize();
+    }
+    #endregion
+
+
+    #region Text
+    public static void SafeSetText(this Text text, string value)
+    {
+        if (text.SafeIsNull())
+            return;
+
+        text.text = value;
+    }
+
+    public static void SafeSetText(this TextMeshProUGUI text, string value)
+    {
+        if (text.SafeIsNull())
+            return;
+
+        text.text = value;
+    }
+    #endregion
+}
